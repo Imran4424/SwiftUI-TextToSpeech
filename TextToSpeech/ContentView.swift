bde31ffa5618636f3c67f79a8729f5774900ec98
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-07-22 22:13:00 +0600

    add text to speech

--- TextToSpeech/ContentView.swift
+++ TextToSpeech/ContentView.swift
@@ -5,15 +5,25 @@
 //  Created by Shah Md Imran Hossain on 22/7/23.
 //
 
+import AVFoundation
 import SwiftUI
 
 struct ContentView: View {
+    @State private var givenText = ""
+    @State private var synthesizer = AVSpeechSynthesizer()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("Text", text: $givenText)
+                .padding()
+            
+            Button("Speak") {
+                let utterance = AVSpeechUtterance(string: givenText)
+                utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+                utterance.rate = 0.4
+                
+                synthesizer.speak(utterance)
+            }
         }
         .padding()
     }
