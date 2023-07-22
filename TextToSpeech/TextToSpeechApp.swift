93d6282cb4cfdb815fa781e422ad83efbadf748c
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-07-22 21:27:52 +0600

    project init

---
+++ TextToSpeech/TextToSpeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextToSpeechApp.swift
+//  TextToSpeech
+//
+//  Created by Shah Md Imran Hossain on 22/7/23.
+//
+
+import SwiftUI
+
+@main
+struct TextToSpeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
